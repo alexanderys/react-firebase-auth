c1a3a913bf4c7785e634554b1a9402611adaf346
alexanderys <alexander.yararsellgren@student.kyh.se>
2021-02-19 11:28:18 +0100

    worked on authorization

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
@@ -22,7 +23,10 @@ const withAuthorization = condition => Component => {
 
         render() {
             return (
-                <Component {...this.props} />
+                <AuthUserContext.Consumer>
+                    {authUser =>
+                        condition(authUser) ? <Component {...this.props} /> : null}
+                </AuthUserContext.Consumer>
             );
         }
     }
